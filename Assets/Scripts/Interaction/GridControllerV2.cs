5e6f64fc5dd7a243482eaebaf3f3dea806592ed6
popote <yvan.bladet@gmail.com>
2021-03-17 17:44:47 +0100

    Preparation for the phonebuild

---
+++ Assets/Scripts/Interaction/GridControllerV2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Scripts.Actors;
+using Scripts.Main;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Scripts.Interaction
+{
+    public class GridControllerV2 :MonoBehaviour
+    {
+        public GameObject PrefabCursor;
+        public PlayGridV2 Grid;
+        [Range(0, 50)] public float cursorSmoothFactor = 9f;
+        public MoveActorV2 prefabCube;
+        public inputState InputState;
+        
+        
+        private Camera _camera;
+        private Vector3 _cursorPos = Vector3.zero;
+        private Transform _cursor;
+
+        public enum inputState
+        {
+            none,Actor, Destination
+        }
+        private void Awake()
+        {
+            _camera = Camera.main;
+            _cursor = Instantiate(PrefabCursor, Vector3.zero, quaternion.identity).transform;
+        }
+
+        private void Update()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit)) {
+                Vector2Int selectedCellT = Grid.GetCellByWorldPos(hit.point);
+                Vector2Int selectedCell;
+                if (selectedCellT != new Vector2Int(-1,-1)) {
+                    selectedCell = selectedCellT;
+                    _cursorPos = Grid.GetCellCenterWorldPosByCell(selectedCell);
+                    if (Input.GetButtonDown("Fire1"))
+                    {
+                        //Grid.Cells[selectedCell.x,selectedCell.y].AddMoveValue(10);
+                        if (InputState == inputState.Actor)
+                        {
+                            MoveActorV2 MA= Instantiate(prefabCube, hit.point+new Vector3(0,0.5f,0), quaternion.identity);
+                            MA.grid = Grid;
+                        }
+
+                        if (InputState == inputState.Destination)
+                        {
+                            Grid.originePos = selectedCell;
+                            Grid.CalculateFlowField();
+                            InputState = inputState.none;
+                        }
+                    }
+/*
+                    if (Input.GetButtonDown("Fire2"))
+                    {
+                        Grid.GetCell(selectedCell).Iswall = true;
+                        Grid.Cells[selectedCell.x,selectedCell.y].AddMoveValue(500);
+                    }
+                     if (Input.GetKey("i"))
+                   {
+                       Debug.Log ("la célulle "+selectedCell+" a une move value de "+ Grid.GetCell(selectedCell).MoveValue)
+                   }
+
+                    if (Input.GetKey("a"))
+                    {
+                       MoveActorV2 MA= Instantiate(prefabCube, hit.point+new Vector3(0,0.5f,0), quaternion.identity);
+                       MA.grid = Grid;
+                    }
+                   if (Input.GetButtonDown("Jump"))
+                   {
+                       Grid.originePos = selectedCell;
+                       Grid.CalculateFlowField();
+                   }
+                  */
+                }
+            }
+            _cursor.position = Vector3.Lerp(_cursor.position, _cursorPos, cursorSmoothFactor * Time.deltaTime);
+        }
+
+        public void SetInputStateOnAdd()
+        {
+            InputState = inputState.Actor;
+        }
+
+        public void SetInputStateOnDestination()
+        {
+            InputState = inputState.Destination;
+        }
+    }
+}
---
+++ Assets/Scripts/Main/MoveActorV2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Scripts.Main;
+using UnityEngine;
+
+namespace Scripts.Actors
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class MoveActorV2:MonoBehaviour
+    {
+        public float MaxSpeed=10;
+        public float Acceleration=1;
+        [HideInInspector] public PlayGridV2 grid;
+        private Rigidbody2D _rigidbody;
+        
+        
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2Int pos = new Vector2Int(Mathf.FloorToInt(transform.position.x),Mathf.FloorToInt(transform.position.y));
+            if (grid.CheckIsInGrid(pos))
+            {
+                _rigidbody.AddForce(grid.GetCell(pos).Get2DVector()*Acceleration);
+            }
+            _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, MaxSpeed);
+            _rigidbody.drag = grid.GetCell(pos).DradFactor;
+        }
+    }
+}
